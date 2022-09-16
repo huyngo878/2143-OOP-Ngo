47de4a50c55f618b9e5a2966d987bea42a9e4ae7
HuyNgo01 <90549599+HuyNgo01@users.noreply.github.com>
2022-09-16 05:37:40 -0500

    updated

--- Assignments/P01/main.cpp
+++ Assignments/P01/main.cpp
@@ -1,229 +1,330 @@
 /*
- 
-  $$$$$$\    $$\ $$\   $$\  $$$$$$\                                
- $$  __$$\ $$$$ |$$ |  $$ |$$ ___$$\                               
- \__/  $$ |\_$$ |$$ |  $$ |\_/   $$ |                              
-  $$$$$$  |  $$ |$$$$$$$$ |  $$$$$ /                               
- $$  ____/   $$ |\_____$$ |  \___$$\                               
- $$ |        $$ |      $$ |$$\   $$ |                              
- $$$$$$$$\ $$$$$$\     $$ |\$$$$$$  |                              
- \________|\______|    \__| \______/                               
-                                                                   
-                                                                   
-                                                                   
- $$$$$$$\   $$$$$$\    $$\                                         
- $$  __$$\ $$$ __$$\ $$$$ |                                        
- $$ |  $$ |$$$$\ $$ |\_$$ |                                        
- $$$$$$$  |$$\$$\$$ |  $$ |                                        
- $$  ____/ $$ \$$$$ |  $$ |                                        
- $$ |      $$ |\$$$ |  $$ |                                        
- $$ |      \$$$$$$  /$$$$$$\                                       
- \__|       \______/ \______|                                      
-                                                                   
-                                                                   
-                                                                   
- $$\   $$\                           $$\   $$\                     
- $$ |  $$ |                          $$$\  $$ |                    
- $$ |  $$ |$$\   $$\ $$\   $$\       $$$$\ $$ | $$$$$$\   $$$$$$\  
- $$$$$$$$ |$$ |  $$ |$$ |  $$ |      $$ $$\$$ |$$  __$$\ $$  __$$\ 
+
+  $$$$$$\    $$\ $$\   $$\  $$$$$$\
+ $$  __$$\ $$$$ |$$ |  $$ |$$ ___$$\
+ \__/  $$ |\_$$ |$$ |  $$ |\_/   $$ |
+  $$$$$$  |  $$ |$$$$$$$$ |  $$$$$ /
+ $$  ____/   $$ |\_____$$ |  \___$$\
+ $$ |        $$ |      $$ |$$\   $$ |
+ $$$$$$$$\ $$$$$$\     $$ |\$$$$$$  |
+ \________|\______|    \__| \______/
+
+
+
+ $$$$$$$\   $$$$$$\    $$\
+ $$  __$$\ $$$ __$$\ $$$$ |
+ $$ |  $$ |$$$$\ $$ |\_$$ |
+ $$$$$$$  |$$\$$\$$ |  $$ |
+ $$  ____/ $$ \$$$$ |  $$ |
+ $$ |      $$ |\$$$ |  $$ |
+ $$ |      \$$$$$$  /$$$$$$\
+ \__|       \______/ \______|
+
+
+
+ $$\   $$\                           $$\   $$\
+ $$ |  $$ |                          $$$\  $$ |
+ $$ |  $$ |$$\   $$\ $$\   $$\       $$$$\ $$ | $$$$$$\   $$$$$$\
+ $$$$$$$$ |$$ |  $$ |$$ |  $$ |      $$ $$\$$ |$$  __$$\ $$  __$$\
  $$  __$$ |$$ |  $$ |$$ |  $$ |      $$ \$$$$ |$$ /  $$ |$$ /  $$ |
  $$ |  $$ |$$ |  $$ |$$ |  $$ |      $$ |\$$$ |$$ |  $$ |$$ |  $$ |
  $$ |  $$ |\$$$$$$  |\$$$$$$$ |      $$ | \$$ |\$$$$$$$ |\$$$$$$  |
- \__|  \__| \______/  \____$$ |      \__|  \__| \____$$ | \______/ 
-                     $$\   $$ |                $$\   $$ |          
-                     \$$$$$$  |                \$$$$$$  |          
-                      \______/                  \______/           
- 
+ \__|  \__| \______/  \____$$ |      \__|  \__| \____$$ | \______/
+                     $$\   $$ |                $$\   $$ |
+                     \$$$$$$  |                \$$$$$$  |
+                      \______/                  \______/
+
 */
 
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
 using namespace std;
 
-struct Node
-{
+struct Node {
   int data;
-  Node * next;
+  Node *next;
 
-  Node(int num)
-  {
+  Node(int num) {
     data = num;
     next = NULL;
   }
 };
-class Vector
-{
-private:
-    Node * head;
-    Node * tail;
-public:
-    Vector();
-    Vector(Vector*);
-    void pushFront(int);
-    void print();
-    void pushRear(int);
-    void pushRear(Vector);
-    Vector(int *, int);
 
-  
+class Vector {
+private:
+  Node *head;
+  Node *tail;
+
+public:
+  Vector();
+  Vector(string);
+  Vector(Vector *);
+  void pushFront(int);
+  void print();
+  void pushRear(int);
+  void pushRear(Vector);
+  int popFront();
+  int popRear();
+  void inOrderPush(int);
+  int popAt(int);
+  int find(int);
+  void pushFront(Vector);
+  Vector(int *, int);
 };
 
-Vector::Vector()
-{
-    head = NULL;
-    tail = NULL;
+void openFiles(ifstream &infile, ofstream &outfile) {
+  string inFileName;
+  string outFileName;
+  cout << "Enter the input file name: ";
+  cin >> inFileName;
+  infile.open(inFileName); // open input file
+  cout << "Enter the output file name: ";
+  cin >> outFileName;
+  outfile.open(outFileName); // open out putfile
 }
 
-Vector::Vector(int *_array, int size)
-{
-   for(int i = size; i >= 0; i--)
-   {
-        if(i == size){}
-        else
-        pushFront(_array[i]);
-   }
+Vector::Vector(string fname) {
+  ifstream infile;
+  ofstream outfile;
+  openFiles(infile, outfile);
+  int num;
+  while (!infile.eof()) {
+    infile >> num;
+    pushFront(num);
+  }
 }
 
-Vector::Vector(Vector * _Vector)
-{
-    Node * firstList = head;
-    Node * secondList = _Vector->head;
+Vector::Vector() {
+  head = NULL;
+  tail = NULL;
 }
 
-void Vector::pushRear(Vector _vector)
-{
-    Node * firstList = head;
-    Node * secondList = _vector.head;
+Vector::Vector(int *_array, int size) {
+  for (int i = size; i >= 0; i--) {
+    if (i == size) {
+    } else
+      pushFront(_array[i]);
+  }
+}
 
-    if(head == NULL)
-    {
-        head = secondList;
-        tail = secondList;
-    }
-    else
-    {
-        tail->next = secondList;
-    }
+Vector::Vector(Vector *_Vector) {
+  Node *firstList = head;
+  Node *secondList = _Vector->head;
+}
+
+void Vector::pushRear(Vector _vector) {
+  Node *firstList = head;
+  Node *secondList = _vector.head;
+
+  if (head == NULL) {
+    head = secondList;
     tail = secondList;
+  } else {
+    tail->next = secondList;
+  }
+  tail = secondList;
 }
 
-void Vector::pushRear(int num)
-{
-    Node * temp = new Node(num);
+void Vector::pushRear(int num) {
+  Node *temp = new Node(num);
 
-    if(head == NULL)
-    {
-        head = temp;
-        tail = temp;
-    }
-    else
-    {
-        tail->next = temp;
-    }
+  if (head == NULL) {
+    head = temp;
     tail = temp;
+  } else {
+    tail->next = temp;
+  }
+  tail = temp;
 }
 
-void Vector::pushFront(int n)
-{
-    Node * newNode = new Node(n);
-    if(head == NULL)
+void Vector::pushFront(int n) {
+  Node *newNode = new Node(n);
+  if (head == NULL) {
+    head = newNode;
+    tail = newNode;
+  } else {
+    newNode->next = head;
+    head = newNode;
+  }
+}
+
+int Vector::popFront() {
+  Node *temp = head;
+  head = head->next;
+  delete temp;
+  return head->data;
+}
+
+void Vector::pushFront(Vector _vector) {
+  Node *firstList = head;
+  Node *secondList = _vector.head;
+
+  if (head == NULL) {
+    head = secondList;
+    tail = secondList;
+  } else {
+    head->next = secondList;
+  }
+  tail = secondList;
+}
+
+int Vector::popRear() {
+  Node *temp = head;
+  while (temp->next->next != NULL) {
+    temp = temp->next;
+  }
+  tail = temp;
+  int deletedNum = tail->next->data;
+  delete tail->next;
+  tail->next = NULL;
+  return deletedNum;
+}
+
+void Vector::print() {
+  Node *traverse = head;
+  cout << "[" ;
+    while (traverse != NULL) {
+    cout << traverse->data;
+    traverse = traverse->next;
+    if(traverse == NULL)
     {
-        head = newNode;
-        tail = newNode;
+      cout << "] ";
     }
     else
-    {
-        newNode->next = head;
-        head = newNode;
-    }
+    cout << ", ";
 
-
+  }
+  cout << endl;
 }
 
-void Vector::print()
-{
-    Node * traverse = head;
-    while(traverse != NULL)
-    {
-        cout << traverse->data << " ";
+int Vector::find(int num) {
+  // Used to keep track of the Node Index
+  int index = 0;
+  Node *temp = head;
+  // LinkedList traversal for finding the node
+  while (temp != NULL) {
+    if (temp->data == num) {
+      // If element found return index
+      return index;
+    }
+    temp = temp->next;
+    index++;
+  }
+  // If element not found
+  return -1;
+}
+
+void Vector::inOrderPush(int num) {
+  // Dynamically create your new node
+  Node *temp = new Node(num);
+
+  // if empty
+  if (head == NULL) {
+    head = temp;
+    tail = temp;
+  } else {
+
+    Node *traverse = head;
+
+    // if first number is smaller than num
+    if (traverse->data > num) {
+      pushFront(num);
+    } else {
+
+      while (traverse->next != NULL && (traverse->next->data < num)) {
         traverse = traverse->next;
+      }
+
+      if (traverse->next == NULL) {
+        pushFront(num);
+      } else {
+        // connecting the node to the rest of the list
+        temp->next = traverse->next;
+        traverse->next = temp;
+      }
     }
-    cout << endl;
+  }
 }
 
+int main() {
+  int x = 0;
 
-int main()
-{
-    int x = 0;
+  Vector v1;
+  v1.pushFront(18);
+  v1.pushFront(20);
+  v1.pushFront(25);
+  v1.pushRear(18);
+  v1.pushRear(20);
+  v1.pushRear(25);
+  v1.print();
+  // [25, 20, 18, 18, 20, 25]
 
-    Vector v1;
-    v1.pushFront(18);
-    v1.pushFront(20);
-    v1.pushFront(25);
-    v1.pushRear(18);
-    v1.pushRear(20);
-    v1.pushRear(25);
-    v1.print();
-// [25, 20, 18, 18, 20, 25]
+  int A[] = {11, 25, 33, 47, 51};
+  Vector v2(A, 5);
+  v2.print();
+  // [11, 25, 33, 47, 51]
 
-int A[] = {11,25,33,47,51};
-Vector v2(A,5);
-v2.print();
-// [11, 25, 33, 47, 51]
+  v2.pushFront(9);
+  // v2.inOrderPush(27);
+  v2.pushRear(63);
+  v2.print();
+  // [9, 11, 25, 33, 47, 51, 63]
 
-v2.pushFront(9);
-//v2.inOrderPush(27);
-v2.pushRear(63);
-v2.print();
-// [9, 11, 25, 33, 47, 51, 63]
+  v1.pushRear(v2);
+  v1.print();
+  v2.print();
+  // [25, 20, 18, 18, 20, 25, 9, 11, 25, 33, 47, 51, 63]
 
-v1.pushRear(v2);
-v1.print();
-// [25, 20, 18, 18, 20, 25, 9, 11, 25, 33, 47, 51, 63]
-/*
-x = v1.popFront();
-x = v1.popFront();
-x = v1.popFront();
-v1.print();
-// [18, 20, 25, 9, 11, 25, 27, 33, 47, 51, 63]
-cout<<x<<endl;
-// 18
+  x = v1.popFront();
+  x = v1.popFront();
+  x = v1.popFront();
+  v1.print();
+  v2.print();
+  // [18, 20, 25, 9, 11, 25, 27, 33, 47, 51, 63]
+  cout << x << endl;
+  // 18
 
-x = v1.popRear();
-x = v1.popRear();
-x = v1.popRear();
-v1.print();
-// [18, 20, 25, 9, 11, 25, 27, 33]
-cout<<x<<endl;
-// 47
+  x = v1.popRear();
+  x = v1.popRear();
+  x = v1.popRear();
+  v2.print();
+  v1.print();
 
-x = v2.popAt(3);
-v2.print();
-// [9, 11, 25, 33, 47, 51, 63]
-cout<<x<<endl;
-// 27
+  // [18, 20, 25, 9, 11, 25, 27, 33]
+  cout << x << endl;
+  // 47
 
-x = v2.find(51);
-cout<<x<<endl;
-// 5
+  // x = v2.popAt(3);
+  // v2.print();
+  // [9, 11, 25, 33, 47, 51, 63]
+  // cout << x << endl;
 
-x = v2.find(99);
-cout<<x<<endl;
-// -1
+  // 27
+  x = v2.find(51);
+  v2.print();
+  cout << x << endl;
+  // 5
 
-Vector v3(v1);
-v3.print();
-// [18, 20, 25, 9, 11, 25, 27, 33]
+  x = v2.find(25);
+  cout << x << endl;
+  // -1
 
-v3.pushFront(v2);
-v3.print();
-//[9, 11, 25, 33, 47, 51, 63, 18, 20, 25, 9, 11, 25, 27, 33]
+  Vector v3(v1);
+  v3.print();
+  // [18, 20, 25, 9, 11, 25, 27, 33]
 
-Vector v4("input.dat");
-v4.pushRear(v3);
-v4.print();
-// [56, 61, 97, 66, 83, 25, 26, 11, 53, 49, 62, 18, 10, 18, 14, 3, 4, 23, 18, 24, 26, 27, 54, 14, 12, 45, 65, 98, 56, 97, 15, 84, 98, 9, 11, 25, 33, 47, 51, 63, 18, 20, 25, 9, 11, 25, 27, 33]
-*/
-    return 0;
+  v3.pushFront(v2);
+  v3.print();
+  cout << " here" << endl;
+  //[9, 11, 25, 33, 47, 51, 63, 18, 20, 25, 9, 11, 25, 27, 33]
+
+  Vector v4("input.dat");
+  v4.print();
+  // v4.pushRear(v3);
+  // [56, 61, 97, 66, 83, 25, 26, 11, 53, 49, 62, 18, 10, 18, 14, 3, 4, 23,
+  // 18, 24, 26, 27, 54, 14, 12, 45, 65, 98, 56, 97, 15, 84, 98, 9, 11, 25, 33,
+  // 47, 51, 63, 18, 20, 25, 9, 11, 25, 27, 33]
+  //
+  return 0;
 }
