c320c00d28542db597c52e525f102ade75cb3fc0
Huy Ngo <huyngo878@yahoo.com>
2022-11-14 14:51:31 -0600

    Added main files

---
+++ Assignments/P05/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <random>
+#include <string>
+#include <vector>
+
+#include "dice.hpp"
+#include "helpers.hpp"
+#include "weapon.hpp"
+#include "fighter.hpp"
+
+using namespace std;
+
+int main()
+{
+
+
+
+
+
+
+
+    
+    return 0;
+}
